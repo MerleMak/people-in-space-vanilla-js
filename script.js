1013a812623cb2464497bad9ca14ab1e506a28dd
Merle Makoben <merle.makoben@neuefische.de>
2023-12-18 15:43:25 +0100

    Add buttons for each space craft

--- script.js
+++ script.js
@@ -1,30 +1,66 @@
+const crafts = [
+  { craft: 'ISS', name: 'ISS' },
+  { craft: 'Tiangong', name: 'Tiangong' },
+  { craft: 'All', name: 'All' },
+];
+
 const peopleInSpace = document.querySelector('[data-js="people-in-space"]');
 
-async function fetchPeopleInSpace() {
+function craftButton(craft, name) {
+  const button = document.createElement('button');
+  button.textContent = name;
+  button.type = 'button';
+  button.addEventListener('click', () => {
+    renderAstronauts(craft);
+  });
+  return button;
+}
+
+crafts.forEach(({ craft, name }) => {
+  const button = craftButton(craft, name);
+  document.body.append(button);
+});
+
+async function renderAstronauts(craft) {
+  const peoplePerCraft = await fetchPeopleInSpace(craft);
+  const numberOfPeoplePerCraft = peoplePerCraft.length;
+  if (numberOfPeoplePerCraft === 0) {
+    peopleInSpace.textContent = `There are no people on ${craft} right now`;
+  } else {
+    if (craft === 'All') {
+      peopleInSpace.textContent = `People in Space right now: ${numberOfPeoplePerCraft}`;
+    }
+    if (craft === 'ISS') {
+      peopleInSpace.textContent = `People on ISS right now: ${numberOfPeoplePerCraft}`;
+    }
+    if (craft === 'Tiangong') {
+      peopleInSpace.textContent = `People on Tiangong right now: ${numberOfPeoplePerCraft}`;
+    }
+  }
+
+  const existingList = document.querySelector('ul');
+  existingList?.remove();
+
+  const listOfAstronauts = document.createElement('ul');
+  peoplePerCraft.forEach((person) => {
+    const nameofAstronaut = document.createElement('li');
+    nameofAstronaut.textContent = person.name;
+    listOfAstronauts.appendChild(nameofAstronaut);
+  });
+  document.body.appendChild(listOfAstronauts);
+}
+
+async function fetchPeopleInSpace(craft) {
   try {
     const response = await fetch('http://api.open-notify.org/astros.json');
     if (!response.ok) {
       throw new Error(response.status);
     }
-
     const peopleInSpace = await response.json();
-    return peopleInSpace;
+    return craft === 'All'
+      ? peopleInSpace.people
+      : peopleInSpace.people.filter((astronaut) => astronaut.craft === craft);
   } catch (error) {
-    alert(`Es ist leider ein Fehler aufgetreten: ${error.message}`);
+    alert(`Error: ${error.message}`);
   }
 }
-
-async function renderAstronauts() {
-  const data = await fetchPeopleInSpace();
-  peopleInSpace.textContent = data.number;
-
-  const listOfAstronauts = document.createElement('ul');
-  data.people.map((person) => {
-    const nameofAstronaut = document.createElement('li');
-    nameofAstronaut.textContent = person.name;
-    listOfAstronauts.append(nameofAstronaut);
-  });
-  document.body.append(listOfAstronauts);
-}
-
-renderAstronauts();
